35e83376744d8f31e7a29469c08633124d74bbab
AlcatrazAlexander <51tia2@mail.ru>
2017-12-02 22:37:59 +0200

    Исправлено выравнивание текста

--- BestMusonDownloader/ProggressBarWithLabel.cs
+++ BestMusonDownloader/ProggressBarWithLabel.cs
@@ -87,7 +87,6 @@ namespace BestMusonDownloader
 
         [Browsable(true)]
         [DefaultValue(false)]
-        //[RefreshProperties(RefreshProperties.Repaint)]
         public bool UsePercentage
         {
             get => _usePercentage;
@@ -108,6 +107,7 @@ namespace BestMusonDownloader
 
             _label.ForeColor = Color.Black;
             this.ForeColor = SystemColors.Highlight;
+            _label.SizeChanged += _label_SizeChanged;
             SetLabelText();
         }
 
@@ -118,15 +118,15 @@ namespace BestMusonDownloader
             // 
             // _label
             // 
-            this._label.Anchor = System.Windows.Forms.AnchorStyles.None;
             this._label.AutoSize = true;
             this._label.BackColor = System.Drawing.Color.Transparent;
             this._label.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this._label.Location = new System.Drawing.Point(129, 15);
+            this._label.Location = new System.Drawing.Point(136, 11);
             this._label.Name = "_label";
             this._label.Size = new System.Drawing.Size(42, 15);
             this._label.TabIndex = 0;
             this._label.Text = "0/100";
+            this._label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // ProggressBarWithLabel
             // 
@@ -170,14 +170,19 @@ namespace BestMusonDownloader
                 _label.Text = $"{_value}%";
             else
                 _label.Text = $"{_value}/{_maximum}";
-            SetLabelLocation();
         }
 
         private void SetLabelLocation()
         {
             _label.Location = new Point(
-                this.Width / 2 - _label.Width / 2,
-                this.Height / 2 - _label.Height / 2);
+                (this.Width - _label.Width) / 2,
+                (this.Height - _label.Height) / 2);
         }
+
+        private void _label_SizeChanged(object sender, EventArgs e)
+        {
+            SetLabelLocation();
+        }
+
     }
 }
